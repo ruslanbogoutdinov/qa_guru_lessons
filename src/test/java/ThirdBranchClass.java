cc3cf3189dc2380adc13446857c72234afe88d88
Ruslan Bogoutdinov <rusyabogoutdinov97@gmail.com>
2024-03-01 20:34:09 +0500

    Adding 'ThirdBranchClass'

---
+++ src/test/java/ThirdBranchClass.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+public class ThirdBranchClass {
+    @Test
+    public void calculateSomeNumbers(){
+        int y = 0;
+        for (int i = 0; i < 10; i++) {
+            y += i;
+        }
+        System.out.println("Total amount is: "+y);
+    }
+}
